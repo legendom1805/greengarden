6291604566b06f779878f1c777bb49a62cedb7ae
Om Rawat <omrawat1805@gmail.com>
2024-12-19 01:12:52 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
